f33351045a409b510748239aea9ef93822f8d904
Fajar Rian <fajarrians578@gmail.com>
2023-03-04 12:58:24 +0700

    04032023

--- app/Http/Controllers/SalesInvoiceRecapController.php
+++ app/Http/Controllers/SalesInvoiceRecapController.php
@@ -181,33 +181,59 @@ class SalesInvoiceRecapController extends Controller
         $tblStock1 = "
         <table cellspacing=\"0\" cellpadding=\"1\" border=\"1\" width=\"100%\">
             <tr>
-                <td width=\"10%\"><div style=\"text-align: center; font-weight: bold\">No</div></td>
-                <td width=\"15%\"><div style=\"text-align: center; font-weight: bold\">Tanggal</div></td>
-                <td width=\"15%\"><div style=\"text-align: center; font-weight: bold\">Tunai</div></td>
-                <td width=\"15%\"><div style=\"text-align: center; font-weight: bold\">Piutang</div></td>
-                <td width=\"15%\"><div style=\"text-align: center; font-weight: bold\">Gopay</div></td>
-                <td width=\"15%\"><div style=\"text-align: center; font-weight: bold\">Ovo</div></td>
-                <td width=\"15%\"><div style=\"text-align: center; font-weight: bold\">Shopeepay</div></td>
+                <td width=\"9%\"><div style=\"text-align: center; font-weight: bold\">No</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Tanggal</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Tunai</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Piutang</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Gopay</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Ovo</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Shopeepay</div></td>
+                <td width=\"13%\"><div style=\"text-align: center; font-weight: bold\">Total</div></td>
             </tr>
         
              ";
 
         $no = 1;
         $tblStock2 =" ";
+        $total_tunai = 0;
+        $total_piutang = 0;
+        $total_gopay = 0;
+        $total_ovo = 0;
+        $total_shopee = 0;
+        $total = 0;
+
         for ($i=0; $i <= $count_date ; $i++) { 
             $tblStock2 .= "
             <tr nobr=\"true\">
-                <td width=\"10%\"><div style=\"text-align: center;\">".$no++.".</div></td>
-                <td width=\"15%\"><div style=\"text-align: center;\">".date('d-m-Y', strtotime("+".$i." days", strtotime($start_date)))."</div></td>
-                <td width=\"15%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1),2,'.',',')."</div></td>
-                <td width=\"15%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2),2,'.',',')."</div></td>
-                <td width=\"15%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3),2,'.',',')."</div></td>
-                <td width=\"15%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4),2,'.',',')."</div></td>
-                <td width=\"15%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5),2,'.',',')."</div></td>
+                <td width=\"9%\"><div style=\"text-align: center;\">".$no++.".</div></td>
+                <td width=\"13%\"><div style=\"text-align: center;\">".date('d-m-Y', strtotime("+".$i." days", strtotime($start_date)))."</div></td>
+                <td width=\"13%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1),2,'.',',')."</div></td>
+                <td width=\"13%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2),2,'.',',')."</div></td>
+                <td width=\"13%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3),2,'.',',')."</div></td>
+                <td width=\"13%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4),2,'.',',')."</div></td>
+                <td width=\"13%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5),2,'.',',')."</div></td>
+                <td width=\"13%\"><div style=\"text-align: right;\">".number_format($this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5),2,'.',',')."</div></td>
             </tr>
             ";
+
+            $total_tunai += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1);
+            $total_piutang += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2);
+            $total_gopay += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3);
+            $total_ovo += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4);
+            $total_shopee += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5);
+            $total += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5);
         }
+
         $tblStock3 = " 
+        <tr nobr=\"true\">
+            <td width=\"22%\"><div style=\"text-align: center; font-weight: bold;\">TOTAL</div></td>
+            <td width=\"13%\"><div style=\"text-align: right; font-weight: bold;\">".number_format($total_tunai,2,'.',',')."</div></td>
+            <td width=\"13%\"><div style=\"text-align: right; font-weight: bold;\">".number_format($total_piutang,2,'.',',')."</div></td>
+            <td width=\"13%\"><div style=\"text-align: right; font-weight: bold;\">".number_format($total_gopay,2,'.',',')."</div></td>
+            <td width=\"13%\"><div style=\"text-align: right; font-weight: bold;\">".number_format($total_ovo,2,'.',',')."</div></td>
+            <td width=\"13%\"><div style=\"text-align: right; font-weight: bold;\">".number_format($total_shopee,2,'.',',')."</div></td>
+            <td width=\"13%\"><div style=\"text-align: right; font-weight: bold;\">".number_format($total,2,'.',',')."</div></td>
+        </tr>
         </table>";
 
         $pdf::writeHTML($tblStock1.$tblStock2.$tblStock3, true, false, false, false, '');
@@ -260,15 +286,16 @@ class SalesInvoiceRecapController extends Controller
             $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(40);
             $spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(40);
             $spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(40);
+            $spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(40);
 
     
-            $spreadsheet->getActiveSheet()->mergeCells("B1:H1");
+            $spreadsheet->getActiveSheet()->mergeCells("B1:I1");
             $spreadsheet->getActiveSheet()->getStyle('B1')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
             $spreadsheet->getActiveSheet()->getStyle('B1')->getFont()->setBold(true)->setSize(16);
-            $spreadsheet->getActiveSheet()->getStyle('B3:H3')->getFont()->setBold(true);
+            $spreadsheet->getActiveSheet()->getStyle('B3:I3')->getFont()->setBold(true);
 
-            $spreadsheet->getActiveSheet()->getStyle('B3:H3')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
-            $spreadsheet->getActiveSheet()->getStyle('B3:H3')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+            $spreadsheet->getActiveSheet()->getStyle('B3:I3')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+            $spreadsheet->getActiveSheet()->getStyle('B3:I3')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
 
             $sheet->setCellValue('B1',"Recap Penjualan Dari Periode ".date('d M Y', strtotime($start_date))." s.d. ".date('d M Y', strtotime($end_date)));	
             $sheet->setCellValue('B3',"No");
@@ -278,38 +305,73 @@ class SalesInvoiceRecapController extends Controller
             $sheet->setCellValue('F3',"Gopay");
             $sheet->setCellValue('G3',"ovo");
             $sheet->setCellValue('H3',"Shopeepay");
+            $sheet->setCellValue('I3',"Total");
             
             $j=4;
             $no=0;
-            $totalitem = 0;
-            $totalamount = 0;
+            $total_tunai = 0;
+            $total_piutang = 0;
+            $total_gopay = 0;
+            $total_ovo = 0;
+            $total_shopee = 0;
+            $total = 0;
             
-            foreach($data as $key=>$val){
+            for ($i=0; $i <= $count_date ; $i++) { 
+                $sheet = $spreadsheet->getActiveSheet(0);
+                $spreadsheet->getActiveSheet()->setTitle("Recap Penjualan");
+                $spreadsheet->getActiveSheet()->getStyle('B'.$j.':I'.$j)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+    
+                $spreadsheet->getActiveSheet()->getStyle('D'.$j.':I'.$j)->getNumberFormat()->setFormatCode('0.00');
+        
+                $spreadsheet->getActiveSheet()->getStyle('B'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $spreadsheet->getActiveSheet()->getStyle('C'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $spreadsheet->getActiveSheet()->getStyle('D'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+                $spreadsheet->getActiveSheet()->getStyle('E'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+                $spreadsheet->getActiveSheet()->getStyle('F'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+                $spreadsheet->getActiveSheet()->getStyle('G'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+                $spreadsheet->getActiveSheet()->getStyle('H'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+                $spreadsheet->getActiveSheet()->getStyle('I'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+    
+                $no++;
+                $sheet->setCellValue('B'.$j, $no);
+                $sheet->setCellValue('C'.$j, date('d-m-Y', strtotime("+".$i." days", strtotime($start_date))));
+                $sheet->setCellValue('D'.$j, $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1));
+                $sheet->setCellValue('E'.$j, $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2));
+                $sheet->setCellValue('F'.$j, $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3));
+                $sheet->setCellValue('G'.$j, $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4));
+                $sheet->setCellValue('H'.$j, $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5));
+                $sheet->setCellValue('I'.$j, $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5));
 
-                if(is_numeric($key)){
-                    
-                    $sheet = $spreadsheet->getActiveSheet(0);
-                    $spreadsheet->getActiveSheet()->setTitle("Recap Penjualan");
-                    $spreadsheet->getActiveSheet()->getStyle('B'.$j.':D'.$j)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
-
-                    $spreadsheet->getActiveSheet()->getStyle('D'.$j)->getNumberFormat()->setFormatCode('0.00');
-            
-                    $spreadsheet->getActiveSheet()->getStyle('B'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                    $spreadsheet->getActiveSheet()->getStyle('C'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-                    $spreadsheet->getActiveSheet()->getStyle('D'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
-
-                    $no++;
-                    $sheet->setCellValue('B'.$j, $no);
-                    $sheet->setCellValue('C'.$j, $val);
-                    $sheet->setCellValue('D'.$j, $this->getAmount($key));
-
-                }else{
-                    continue;
-                }
                 $j++;
+                $total_tunai += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1);
+                $total_piutang += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2);
+                $total_gopay += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3);
+                $total_ovo += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4);
+                $total_shopee += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5);
+                $total += $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 1) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 2) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 3) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 4) + $this->getAmount(date('Y-m-d', strtotime("+".$i." days", strtotime($start_date))), 5);
             }
 
-            $spreadsheet->getActiveSheet()->mergeCells('B'.$j.':D'.$j);
+            $spreadsheet->getActiveSheet()->getStyle('B'.$j.':I'.$j)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+            $spreadsheet->getActiveSheet()->getStyle('D'.$j.':I'.$j)->getNumberFormat()->setFormatCode('0.00');
+            $spreadsheet->getActiveSheet()->mergeCells('B'.$j.':C'.$j);
+            $spreadsheet->getActiveSheet()->getStyle('B'.$j.':I'.$j)->getFont()->setBold(true);
+            $spreadsheet->getActiveSheet()->getStyle('B'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+            $spreadsheet->getActiveSheet()->getStyle('D'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            $spreadsheet->getActiveSheet()->getStyle('E'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            $spreadsheet->getActiveSheet()->getStyle('F'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            $spreadsheet->getActiveSheet()->getStyle('G'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            $spreadsheet->getActiveSheet()->getStyle('H'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            $spreadsheet->getActiveSheet()->getStyle('I'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            $sheet->setCellValue('B'.$j, 'TOTAL');
+            $sheet->setCellValue('D'.$j, $total_tunai);
+            $sheet->setCellValue('E'.$j, $total_piutang);
+            $sheet->setCellValue('F'.$j, $total_gopay);
+            $sheet->setCellValue('G'.$j, $total_ovo);
+            $sheet->setCellValue('H'.$j, $total_shopee);
+            $sheet->setCellValue('I'.$j, $total);
+
+            $j++;
+            $spreadsheet->getActiveSheet()->mergeCells('B'.$j.':I'.$j);
             $spreadsheet->getActiveSheet()->getStyle('B'.$j)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
             $sheet->setCellValue('B'.$j, Auth::user()->name.", ".date('d-m-Y H:i'));
             
